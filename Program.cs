d7e750dde27ccd563c5e114cc4ff817d153f6353
FreddieEmms <149086673+FreddieEmms@users.noreply.github.com>
2023-11-12 12:27:09 +0000

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿
+
+//This should print on each line the sentences within the speech marks
+
+Console.WriteLine("How are you?");
+Console.WriteLine("I am very good!");
+Console.WriteLine("That's great.");
